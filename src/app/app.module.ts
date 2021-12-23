1bccec905105e57a93424a3cc3522da531584eb0
dimangchou <dimang12@gmail.com>
2021-12-23 04:49:12 -0800

    add routes into project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { AppRoutingModule } from './app-routing.module';
+
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { environment } from '../environments/environment';
+import { TopNavigationComponent } from './layout/top-navigation/top-navigation.component';
+import { LeftNavigationComponent } from './layout/left-navigation/left-navigation.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavigationComponent,
+    LeftNavigationComponent,
+    LoginComponent,
+    RegisterComponent,
+    LogoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    FlexLayoutModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAX_EH1Zeac4hAft1vEWyTMp50Zl7oA7qE",
+    authDomain: "angular-testing-23637.firebaseapp.com",
+    databaseURL: "https://angular-testing-23637.firebaseio.com",
+    projectId: "angular-testing-23637",
+    storageBucket: "angular-testing-23637.appspot.com",
+    messagingSenderId: "466380770098",
+    appId: "1:466380770098:web:1646a78336a7a26c95652b"
+  }
 };
 
 /*
