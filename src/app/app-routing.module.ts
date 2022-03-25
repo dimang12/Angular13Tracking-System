d3fc2020d8afeaf8ef9bf5b09d4e619285de92eb
dimangchou <dimang12@gmail.com>
2022-03-25 05:51:39 -0700

    Tiny changes on login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,22 @@ import { AppComponent} from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LogoutComponent } from './auth/logout/logout.component';
-import { DashboardComponent} from './home/dashboard/dashboard.component'
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component'
+import { TaskComponent } from './task/task.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'logout', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: '', component: DashboardComponent},
+  {path: 'project', component: ProjectComponent},
+  {path: 'project-detail/:id', component: ProjectDetailComponent},
+  {path: 'task', component: TaskComponent},
+  {path: 'home', component: DashboardComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider'
+import { MatListModule } from '@angular/material/list'
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -11,6 +14,8 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatChipsModule} from '@angular/material/chips';
 // import {MatDatepicker} from '@angular/material/datepicker';
 // import {MatFormField} from '@angular/material/form-field';
 
@@ -28,6 +33,10 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LogoutComponent } from './auth/logout/logout.component';
 import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { ProjectComponent } from './project/project.component';
+import { TaskComponent } from './task/task.component';
+import { BreadcrumbComponent } from './layout/breadcrumb/breadcrumb.component';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
 
 
 
@@ -40,7 +49,11 @@ import { DashboardComponent } from './home/dashboard/dashboard.component';
     LoginComponent,
     RegisterComponent,
     LogoutComponent,
-    DashboardComponent
+    DashboardComponent,
+    ProjectComponent,
+    TaskComponent,
+    BreadcrumbComponent,
+    ProjectDetailComponent,
   ],
   imports: [
     BrowserModule,
@@ -52,6 +65,11 @@ import { DashboardComponent } from './home/dashboard/dashboard.component';
     MatMenuModule,
     MatButtonModule,
     MatInputModule,
+    MatCheckboxModule,
+    MatDividerModule,
+    MatListModule,
+    MatProgressBarModule,
+    MatChipsModule,
     // MatDatepicker,
     FormsModule,
     ReactiveFormsModule
