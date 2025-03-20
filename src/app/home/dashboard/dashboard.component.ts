2c22d85650669119aca7c637f5f61fff109d51d6
dimangchou <dchou@sitscape.com>
2025-03-19 19:40:56 -0700

    Enhance dashboard with project data and improve status pipe

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { TaskInterface } from '../../interfaces/task.interface';
 import { Observable } from 'rxjs';
+import {ProjectInterface} from "../../interfaces/project.interface";
+import {ProjectService} from "../../services/project.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -10,19 +12,21 @@ import { Observable } from 'rxjs';
 })
 export class DashboardComponent implements OnInit {
   public tasks: TaskInterface[] = [];
+  public projects: ProjectInterface[] = [];
 
   //TODO: get top on going tasks
 
   constructor(
-    private taskService: TaskService
+    private taskService: TaskService,
+    private projectService: ProjectService,
   ) { }
 
   ngOnInit(): void {
     this.taskService.getTasksByStatus(['1','2'], 5).subscribe( tasks => {
         this.tasks = tasks;
-      }
-    );
-
+      });
+    this.projectService.getProjects().subscribe( projects => {
+      this.projects = projects;
+    })
   }
-
 }
--- src/app/pipes/status.pipe.ts
+++ src/app/pipes/status.pipe.ts
@@ -5,7 +5,7 @@ import { statusParams } from '../services/params/params.service';
   name: 'status'
 })
 export class StatusPipe implements PipeTransform {
-  transform(value: number): string {
+  transform(value: number | string): string {
     const status = statusParams.find(status => status.value.toString() === value.toString());
     return status ? status.label : 'Unknown';
   }
